fc54f26cdc8083da5ccb904efe153797f09d750b
Ellis Lunnon <ellislunnon@gmail.com>
2024-09-01 14:58:40 +0100

    feat: debounce logic

--- mirror.c
+++ mirror.c
@@ -17,27 +17,34 @@
 // Setup the pins
 #define GPIO_OUT_WARM 19  // warm light
 #define GPIO_OUT_COOL 18  // cool light
-#define GPIO_IN_BUTTON 27 // button in
+#define GPIO_IN_BUTTON 28 // button in
 
-int lamp_state_active_idx = 0;
+#define DEBOUNCE_INTERVAL_US 100000 // 100ms
+absolute_time_t debounce_last_hit = 0;
+
+int lamp_state_active_idx = 2;
 bool lamp_states[4][2] = {
     {OFF, OFF}, // off
     {ON, ON},   // on
     {OFF, ON},  // cool
     {ON, OFF},  // warm
 };
+void lamp_state_next_idx() {
+  lamp_state_active_idx++;
+  lamp_state_active_idx = lamp_state_active_idx % 4;
+}
 
 // Perform initialisation
-int output_init(int pin) {
+int gpio_init_output(int pin) {
   gpio_init(pin);
   gpio_set_dir(pin, GPIO_OUT);
   gpio_set_pulls(pin, false, true); // pull-down
   return PICO_OK;
 }
 
-int input_init(int pin) {
+int gpio_init_input(int pin) {
   gpio_init(pin);
-  gpio_set_dir(GPIO_IN_BUTTON, GPIO_IN);
+  gpio_set_dir(pin, GPIO_IN);
   gpio_set_pulls(pin, false, true); // pull-down
   return PICO_OK;
 }
@@ -48,23 +55,29 @@ void set_state(bool state[2]) {
 }
 
 void irq_handle_state_shift(uint gpio, uint32_t event_mask) {
-  if (gpio == GPIO_IN_BUTTON) {
-    lamp_state_active_idx++;
-    lamp_state_active_idx = lamp_state_active_idx % 4;
-    set_state(lamp_states[lamp_state_active_idx]);
+  absolute_time_t now = get_absolute_time();
+  absolute_time_t debounce_window = debounce_last_hit + DEBOUNCE_INTERVAL_US;
+  if (debounce_window > now) {
+    return;
   }
+
+  if (gpio != GPIO_IN_BUTTON) {
+    return;
+  }
+  debounce_last_hit = now;
+  lamp_state_next_idx();
+  set_state(lamp_states[lamp_state_active_idx]);
 }
 
 int main() {
-  output_init(GPIO_OUT_WARM);
-  output_init(GPIO_OUT_COOL);
-  input_init(GPIO_IN_BUTTON);
+  gpio_init_output(GPIO_OUT_WARM);
+  gpio_init_output(GPIO_OUT_COOL);
+  gpio_init_input(GPIO_IN_BUTTON);
 
   set_state(lamp_states[lamp_state_active_idx]);
 
-  gpio_set_irq_enabled_with_callback(GPIO_IN_BUTTON,
-                                     GPIO_IRQ_EDGE_FALL & GPIO_IRQ_EDGE_RISE,
-                                     true, &irq_handle_state_shift);
+  gpio_set_irq_enabled_with_callback(GPIO_IN_BUTTON, GPIO_IRQ_EDGE_RISE, true,
+                                     &irq_handle_state_shift);
 
   while (true) {
     sleep_ms(UINT32_MAX);
